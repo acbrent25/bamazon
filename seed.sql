8de0113efd7e12f4ec61a07ce3207826e5db157d
Adam Champagne <adamchampagne@gmail.com>
2017-10-14 15:38:59 -0400

    added schema and seeds

---
+++ seed.sql
@@ -0,0 +1,5 @@
+USE bamazonDB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("13pc Mess Kit", "Camp Kitchen", 25.95, 10);
+("Life Straw", "Survival", 17.67, 32);
