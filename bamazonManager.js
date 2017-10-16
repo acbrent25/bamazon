2e643027f5597eadf40095d7ae21329eb31ea79c
Adam Champagne <adamchampagne@gmail.com>
2017-10-16 08:04:37 -0400

    add to inventory working

--- bamazonManager.js
+++ bamazonManager.js
@@ -43,7 +43,7 @@ function chooseYourAdventure() {
                 break;
       
               case "Add to Inventory":
-                console.log("Add to Inventory");
+                addInventory();
                 break;
       
               case "Add New Product":
@@ -63,9 +63,12 @@ function displayProducts(){
         if (err) throw console.log("error at displayProducts(): " + err);
         
         for (var i = 0; i < res.length; i++){
-            console.log("ID: " + res[i].id + " | " + " Product Name: " + res[i].product_name + " Department: " + res[i].department_name + " Price: " + res[i].price + " Stock Qty: " + res[i].stock_quantity);
+            console.log("\n\r");
+            console.log("ID: " + res[i].id + " | " + " Product Name: " + res[i].product_name + " | " + " Department: " + res[i].department_name + " | " + " Price: " + res[i].price + " | " + " Stock Qty: " + res[i].stock_quantity);
         }
+        chooseYourAdventure();
     });
+    
 }
 
 
@@ -78,5 +81,65 @@ function viewLowInventory(){
                 console.log("ID: " + res[i].id + " | " + " Product Name: " + res[i].product_name + " Department: " + res[i].department_name + " Price: " + res[i].price + " Stock Qty: " + res[i].stock_quantity);
             } 
         }
+        chooseYourAdventure();
     });
+    
 }
+
+function addInventory() {
+    displayProducts();
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw console.log("connection error: " + err);
+        
+        inquirer.prompt([
+          {
+              name: "id",
+              type: "input",            
+              message: "Enter the ID of the pruduct you wish to add inventory to",
+            },
+            {
+              name: "units",
+              type: "input",
+              message: "How many would you like to add?",
+              validate: function(value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                  return false;
+                }
+          }      
+        ])
+        .then(function(answer) {
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { id: answer.id }, function(err, res) {
+            
+            var dbStock = res[0].stock_quantity;
+            var reqStock = answer.units;
+            var completed = false;
+            var NewDbStock = parseInt(dbStock) + parseInt(reqStock);
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: NewDbStock
+                        },
+                        {
+                            id: answer.id
+                        }
+                    ],
+                    function(error) {
+                        if (error) throw err;
+                        completed === true;
+                        console.log("==============================================");
+                        console.log("\n\r");
+                        console.log("Stock QTY Updated");
+                        console.log("ID: " + res[0].id + " | " + " Product Name: " + res[0].product_name + " Department: " + res[0].department_name + " Price: " + res[0].price + " Stock Qty: " + res[0].stock_quantity)
+                        console.log("\n\r");
+                        console.log("==============================================");
+                    }
+                );
+
+        });
+        });// inquier.prompt
+    });// conection.query  
+  }// addToInventory()
